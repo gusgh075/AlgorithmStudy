b5df846cdeeeb7efeace1f563524b3295318cd81
gusgh075 <gusgh075@naver.com>
2023-10-06 13:51:10 +0900

    G5_BJ14891_톱니바퀴_simulation

--- src/simulation/G5_BJ14891_톱니바퀴.java
+++ src/simulation/G5_BJ14891_톱니바퀴.java
@@ -1,14 +1,15 @@
+package simulation;
+
 import java.awt.*;
 import java.io.*;
-import java.util.*;
+import java.util.Stack;
 
-public class Main {
-    //0은 왼쪽, 1은 오른쪽
+public class G5_BJ14891_톱니바퀴 {
     static int[][] lrindex = new int[4][2];
     static int[][] topni = new int[4][8];
     static boolean[] visit = new boolean[4];
 
-    public static void main(String[] args) throws IOException {
+    public static void sol() throws IOException {
         //G5_14891_톱니바퀴
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -33,14 +34,14 @@ public class Main {
             int dis = Integer.parseInt(info[1]);
             bang[cur] = dis;
             for (int j = cur; j < 3; j++) {
-                if (topni[j][lrindex[j][1]] != topni[j + 1][lrindex[j + 1][0]]) {
+                if (getLeft(j+1)!= getRight(j)) {
                     bang[j + 1] = bang[j] * -1;
                 } else {
                     break;
                 }
             }
             for (int j = cur; j > 0; j--) {
-                if (topni[j][lrindex[j][0]] != topni[j - 1][lrindex[j - 1][1]]) {
+                if (getLeft(j) != getRight(j-1)) {
                     bang[j - 1] = bang[j] * -1;
                 } else {
                     break;
@@ -48,24 +49,37 @@ public class Main {
             }
             for (int j = 0; j < 4; j++) {
                 if (bang[j] == -1) {
-                    lrindex[j][0] = (lrindex[j][0] + 1) % 8;
-                    lrindex[j][1] = (lrindex[j][1] + 1) % 8;
+                    turnLeft(j);
                 }
                 if (bang[j] == 1) {
-                    lrindex[j][0] = (lrindex[j][0] + 7) % 8;
-                    lrindex[j][1] = (lrindex[j][1] + 7) % 8;
+                    turnRight(j);
                 }
             }
         }
         int ans=0;
         for(int i=0;i<4;i++){
-            ans=ans+(topni[i][(lrindex[i][0]+2)%8]==0?0:(int)Math.pow(2,i));
+            ans=ans+(getTop(i)==0?0:(int)Math.pow(2,i));
         }
         bw.write(ans+"");
         bw.flush();
         bw.close();
         br.close();
-
     }
-
+    public static int getLeft(int index){
+        return topni[index][(lrindex[index][0])];
+    }
+    public static int getRight(int index){
+        return topni[index][(lrindex[index][1])];
+    }
+    public static int getTop(int index){
+        return topni[index][(lrindex[index][0]+2)%8];
+    }
+    public static void turnRight(int index){
+        lrindex[index][0] = (lrindex[index][0] + 1) % 8;
+        lrindex[index][1] = (lrindex[index][1] + 1) % 8;
+    }
+    public static void turnLeft(int index){
+        lrindex[index][0] = (lrindex[index][0] + 7) % 8;
+        lrindex[index][1] = (lrindex[index][1] + 7) % 8;
+    }
 }
