40aec70c39c3e39dcdecf936f12ad729928960e6
gusgh075 <gusgh075@naver.com>
2023-11-29 19:46:16 +0900

    11/29

--- src/DP/N으로표현.java
+++ src/DP/N으로표현.java
@@ -17,29 +17,29 @@ import java.util.Set;
  * - 모든 경우의 수를 고려하라
  * - size를 2부터 설정해서, 정답이 1일때의 경우를 생각하지 못해 틀렸었다.
  */
-class Solution {
+class N으로표현 {
     public int solution(int N, int number) {
-        Set[] DP=new Set[9];
+        Set[] DP = new Set[9];
         for (int i = 1; i < DP.length; i++) {
-            DP[i]=new LinkedHashSet<Integer>();
+            DP[i] = new LinkedHashSet<Integer>();
             DP[i].add(Integer.parseInt(Integer.toString(N).repeat(i)));
         }
-        for(int size=1;size<9;size++){
-            for(int front=1;front<size;front++){
-                int back=size-front;
+        for (int size = 1; size < 9; size++) {
+            for (int front = 1; front < size; front++) {
+                int back = size - front;
                 for (Object f : DP[front]) {
                     for (Object b : DP[back]) {
                         int f1 = (int) f;
                         int b1 = (int) b;
-                        DP[size].add(f1+b1);
-                        DP[size].add(f1-b1);
-                        DP[size].add(f1*b1);
-                        if(b1!=0)
-                            DP[size].add(f1/b1);
+                        DP[size].add(f1 + b1);
+                        DP[size].add(f1 - b1);
+                        DP[size].add(f1 * b1);
+                        if (b1 != 0)
+                            DP[size].add(f1 / b1);
                     }
                 }
             }
-            if(DP[size].contains(number)){
+            if (DP[size].contains(number)) {
                 return size;
             }
         }
--- src/DP/정수삼각형.java
+++ src/DP/정수삼각형.java
@@ -1,23 +1,38 @@
 package DP;
 
+/**
+ * 조건
+ * 1. 거쳐간 숫자의 합이 가장 큰 것을 return
+ * 2. 이동방향은 대각선 좌측, 우측 하단
+ * 전략
+ * 1. 일단 각 자리마다 최댓값인 경우를 저장한다
+ * 2. 이는 2차원 배열로 진행한다
+ * 3. 각 자리마다 어떤 값이 가장 큰지 조사한다
+ * 유의사항
+ * - 높이는 500, 삼각형 최대수는 9,999이므로 오류발생하지 않음
+ * - 재귀를 사용하면, 처리해야 할 변수가 늘어나고, 방문한 곳을 다시 방문하게 되며 코드가 늘어진다.
+ */
 class 정수삼각형 {
-    public int solution(int[][] triangle) {
-        int[][] dp=new int[triangle.length][];
-        for(int i=0;i< triangle.length;i++){
-            dp[i]=new int[triangle[i].length];
-        }
-        dp[0][0]=triangle[0][0];
-        for(int i=0;i< triangle.length-1;i++){
-            for(int j=0;j<triangle[i].length;j++){
-                dp[i+1][j]=Math.max(dp[i][j]+triangle[i+1][j],dp[i+1][j]);
-                dp[i+1][j+1]=Math.max(dp[i][j]+triangle[i+1][j+1],dp[i+1][j+1]);
-            }
-        }
 
+    public int solution(int[][] triangle) {
         int answer = 0;
-        for (int i : dp[triangle.length - 1]) {
-            answer=Math.max(answer,i);
+        //재귀를 쓰지 않은 풀이방법
+        //triangle을 DP로 설정
+        for (int layer = 1; layer < triangle.length; layer++) {
+            //첫 index
+            triangle[layer][0] += triangle[layer - 1][0];
+            //두번째부터 마지막전까지의 index
+            for (int index = 1; index < triangle[layer].length - 1; index++) {
+                triangle[layer][index] += Math.max(triangle[layer - 1][index - 1], triangle[layer - 1][index]);
+            }
+            //마지막 index
+            triangle[layer][triangle[layer].length - 1] += triangle[layer - 1][triangle[layer - 1].length - 1];
+        }
+        //마지막 layer 중 최댓값 조사
+        for (int i = 0; i < triangle[triangle.length - 1].length; i++) {
+            answer = Math.max(triangle[triangle.length - 1][i], answer);
         }
         return answer;
     }
+
 }
