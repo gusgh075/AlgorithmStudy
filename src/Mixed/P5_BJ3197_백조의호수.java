100d1faa7eefdbaac273988b9bd918e93e912685
gusgh075 <gusgh075@naver.com>
2025-10-20 18:18:34 +0900

    25-10-16 Hash 푸는중

---
+++ src/Mixed/P5_BJ3197_백조의호수.java
@@ -0,0 +1,113 @@
+package Mixed;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+
+public class P5_BJ3197_백조의호수 {
+    static int[] dx = new int[]{1, 0, -1, 0};
+    static int[] dy = new int[]{0, 1, 0, -1};
+
+    public static class Pair {
+        Point pnt;
+        int cnt;
+
+        public Pair(Point pnt, int cnt) {
+            this.pnt = new Point(pnt.x, pnt.y);
+            this.cnt = cnt;
+        }
+    }
+
+    public static boolean spreadL(int[][] map, Point now) {
+        int cur = map[now.y][now.x];
+        int r = map.length;
+        int c = map[0].length;
+        if (cur == 2 || cur == 3) {
+            Queue<Point> lQ = new ArrayDeque<>();
+            lQ.add(new Point(now.x, now.y));
+            while (!lQ.isEmpty()) {
+                Point n = lQ.poll();
+                for (int i = 0; i < 4; i++) {
+                    int nx = n.x + dx[i];
+                    int ny = n.y + dy[i];
+                    if (ny >= r || nx >= c || ny < 0 || nx < 0) continue;
+                    if (map[ny][nx] == map[n.y][n.x] || map[ny][nx] == 7) continue;
+                    if ((map[ny][nx] == 2 && map[n.y][n.x] == 3) || (map[ny][nx] == 3 && map[n.y][n.x] == 2))
+                        return true;
+                    if (map[ny][nx] == 0) {
+                        map[ny][nx] = map[n.y][n.x];
+                        lQ.add(new Point(nx, ny));
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int r = input[0];
+        int c = input[1];
+        int[][] map = new int[r][c];   //X : 7 / L : 2,3 / . : 0
+        int nowL = 2;
+        Queue<Point> lQ = new ArrayDeque<>();
+        Queue<Pair> q = new ArrayDeque<>();
+        for (int i = 0; i < r; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < c; j++) {
+                char cur = s.charAt(j);
+                if (cur == '.') {
+                    map[i][j] = 0;
+                    q.add(new Pair(new Point(j, i), 0));
+                } else if (cur == 'L') {
+                    map[i][j] = nowL;
+                    nowL++;
+                    lQ.add(new Point(j, i));
+                    q.add(new Pair(new Point(j, i), 0));
+                } else if (cur == 'X') map[i][j] = 7;
+                else throw new IllegalArgumentException("잘못된 문자 입력: " + cur);
+            }
+        }
+        //백조 인근 물들을 표시
+        while (!lQ.isEmpty()) {
+            Point cur = lQ.poll();
+            if(spreadL(map, cur)) {System.out.println(0);return;}
+        }
+        for (Pair now : q) {
+            Point cur=now.pnt;
+            if(spreadL(map,new Point(cur.x,cur.y))) {
+                System.out.println(now.cnt);
+                return;
+            }
+        }
+        int flag=1;
+        while (!q.isEmpty()) {
+            Pair now = q.poll();
+            Point cur = now.pnt;
+            if(flag==now.cnt) {
+                for (Pair p : q) {
+                    Point cp = p.pnt;
+                    if (spreadL(map, new Point(cp.x, cp.y))) {
+                        System.out.println(p.cnt);
+                        return;
+                    }
+                }
+                flag++;
+            }
+            for (int i = 0; i < 4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+                if (ny >= r || nx >= c || ny < 0 || nx < 0) continue;
+                if(map[ny][nx]==7){
+                    map[ny][nx]=map[cur.y][cur.x];
+                    q.add(new Pair(new Point(nx, ny), now.cnt + 1));
+                }
+            }
+        }
+    }
+}
